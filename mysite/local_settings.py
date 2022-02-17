303a0820582a6bc0a54d7623c70f581a6d3121aa
matsumoto <matsumoto.yuji.m@gmail.com>
2022-02-17 22:58:23 +0900

    first commit

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,9 +1,9 @@
 import os
 
+SECRET_KEY = 'jx19rs6ish5%w6^koyhw8!uelw+j!)^cpk&l-m2o5z3il6o7sn'
+
 # settings.pyからそのままコピー
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-SECRET_KEY = 'jx19rs6ish5%w6^koyhw8!uelw+j!)^cpk&l-m2o5z3il6o7sn'
 
 # settings.pyからそのままコピー
 DATABASES = {
