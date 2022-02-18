8487fb738ff6fd3f2f9bd3026235b658ad146927
matsumoto <matsumoto.yuji.m@gmail.com>
2022-02-18 18:49:00 +0900

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMP_DIR = os.path.join(BASE_DIR, 'app/templates/app')
+TEMP_DIR = os.path.join(BASE_DIR, 'app/templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
