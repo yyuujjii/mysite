a3ec5692a6ffc5f6e2162517653fdd4e1e96a235
matsumoto <matsumoto.yuji.m@gmail.com>
2022-02-18 16:55:00 +0900

    first commit

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('contents/', views.contents, name='contents'),
+    path('contact/', views.contact, name='contact'),
     path('contact_form/', views.contact_form, name='contact_form'),
-
+    path('comp/', views.comp, name='comp'),
 ]
