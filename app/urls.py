0e4b2cf1d4069fac7828151fc29aa908da7d80eb
matsumoto <matsumoto.yuji.m@gmail.com>
2022-02-18 15:02:42 +0900

    first commit

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from app import views
+from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('contents/', views.contents, name='contents'),
-    path('contact/', views.contact, name='contact'),
-    path('contact_form/', views.contact_form, name='contact_form'),
-    path('comp/', views.comp, name='comp'),
 ]
